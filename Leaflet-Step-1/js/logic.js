b603318d6b5d9d9c12c8fb08513c9f4bc02cc081
bryanmangrum <bryanspringcreek@gmail.com>
2022-04-24 20:01:26 -0400

    edited URL to contain public API key

--- Leaflet-Step-1/js/logic.js
+++ Leaflet-Step-1/js/logic.js
@@ -5,30 +5,30 @@ var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_we
 
 // Perform a GET request to the query URL/
 d3.json(queryUrl).then(function (data) {
-// Once we get a response, send the data.features object to the createFeatures function.
-createFeatures(data.features);
+  // Once we get a response, send the data.features object to the createFeatures function.
+  createFeatures(data.features);
 });
 
 
-function getColor(depth){
-  if(depth < 10){
+function getColor(depth) {
+  if (depth < 10) {
     return "#77FF33"
-}
-else if(depth<30){
-  return "#6EAD2A"
-}
-else if (depth <50){
+  }
+  else if (depth < 30) {
+    return "#6EAD2A"
+  }
+  else if (depth < 50) {
     return "#F6F92F"
-}
-else if (depth <70){
-  return "#F9AC2F"
-}
-else if (depth <90){
-  return "#F9471B"
-} 
-else {
-  return "#5D1200"
-}
+  }
+  else if (depth < 70) {
+    return "#F9AC2F"
+  }
+  else if (depth < 90) {
+    return "#F9471B"
+  }
+  else {
+    return "#5D1200"
+  }
 };
 
 function createFeatures(earthquakeData) {
@@ -49,9 +49,9 @@ function createFeatures(earthquakeData) {
   // Run the onEachFeature function once for each piece of data in the array.
   var earthquakes = L.geoJSON(earthquakeData, {
     onEachFeature: onEachFeature,
-    pointToLayer: function(feature, latlng) {
+    pointToLayer: function (feature, latlng) {
       return new L.CircleMarker(latlng, {
-        radius: feature.properties.mag*4, 
+        radius: feature.properties.mag * 4,
         fillOpacity: 0.5,
         stroke: true,
         color: 'black',
@@ -68,43 +68,43 @@ function createFeatures(earthquakeData) {
 function createMap(earthquakes) {
 
   // // Create the base layers.
-var gray = L.tileLayer('https://maps.omniscale.net/v2/private-bryan-mangrum-0af0fa70/style.grayscale/{z}/{x}/{y}.png',{
-attribution:  "©️ <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> ©️ <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>"
-});
+  var gray = L.tileLayer('https://maps.omniscale.net/v2/my-grayscale-720e1d78/style.grayscale/{z}/{x}/{y}.png', {
+    attribution: "©️ <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> ©️ <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>"
+  });
 
-var legend = L.control({position: 'bottomright'});
-    legend.onAdd = function (map) {
+  var legend = L.control({ position: 'bottomright' });
+  legend.onAdd = function (map) {
 
     var div = L.DomUtil.create('div', 'info legend');
     labels = ['<strong>Depth of Epicenter</strong>'];
     example = [9, 15, 40, 60, 80, 99];
-    categories = ['< 10','10 - 30','30 - 50','50 - 70','70 - 90', '90+'];
+    categories = ['< 10', '10 - 30', '30 - 50', '50 - 70', '70 - 90', '90+'];
 
     for (var i = 0; i < categories.length; i++) {
 
-            div.innerHTML += 
-            labels.push(
-                '<i class="circle" style="background:' + getColor(example[i]) + '"></i> ' +
-            (categories[i] ? categories[i] : '+'));
+      div.innerHTML +=
+        labels.push(
+          '<i class="circle" style="background:' + getColor(example[i]) + '"></i> ' +
+          (categories[i] ? categories[i] : '+'));
 
-        }
-        div.innerHTML = labels.join('<br>');
+    }
+    div.innerHTML = labels.join('<br>');
     return div;
   };
 
 
-var myMap = L.map("map", {
-  center: [
-    37.09, -95.71
-  ],
-  zoom: 5
-  
+  var myMap = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5
+
   });
 
 
-gray.addTo(myMap);
-earthquakes.addTo(myMap);
-legend.addTo(myMap);
+  gray.addTo(myMap);
+  earthquakes.addTo(myMap);
+  legend.addTo(myMap);
 
   // Create a layer control.
   // Pass it our baseMaps and overlayMaps.
